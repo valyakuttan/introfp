d29592100d19b870f37cbb2a33c7fd98160b4654
valyakuttan <kuttixxxl@gmail.com>
2013-10-19 11:27:44 +0530

    improved readability of Anagrams.hs

--- Anagrams.hs
+++ Anagrams.hs
@@ -1,66 +1,75 @@
 -- Anagrams.hs
 --
 --
+{-# LANGUAGE FlexibleContexts, GeneralizedNewtypeDeriving #-}
 
 import Prelude hiding (subtract)
 import Data.List
 import Data.Char (toLower)
 import Data.Ord (comparing)
-import Control.Monad (mapM_)
 import qualified Data.Map as Map
+import Data.Map (Map)
 import Control.Monad.State
+import Control.Applicative ((<$>))
 
 type Occurrences  = [(Char, Int)]
 
-type Dictionary   = [(Occurrences, [String])]
+type Dictionary   = Map Occurrences [String]
 
-type StateMap a b = State (Map.Map a b) b
+data Memo = M !(Map Occurrences [[String]])
 
-printAnagrams :: String -> IO ()
-printAnagrams sentence = do
-    s <- readFile "resources/linuxwords.txt"
-    let d = dictionary $ concatMap words $ lines s
-    let prints = putStrLn . concat . intersperse " "
-    mapM_ prints $ anagramsM d $ words sentence
+type MemoT = State Memo
 
-anagramsM :: Dictionary -> [String] -> [[String]]
-anagramsM d sentence = memoizeM anagramsM' $ occurrences sentence
-  where
-      anagramsM' :: Monad m => (Occurrences -> m [[String]]) 
-                            -> Occurrences -> m [[String]]
-      anagramsM' a' []   = return [[]]
-      anagramsM' a' occr = return concat `ap` mapM mkAnagramsM combs
-        where
-            mkAnagramsM soccr
-                = return (concatMap (append soccr)) `ap` suffixesM soccr
-            append soccr suffix = map (:suffix) $ prefixes soccr
-            prefixes soccr = maybe [] id $ wordAnagrams soccr d
-            suffixesM soccr = a' $ subtract occr soccr
-            combs = filter (/=[]) $ combinations occr
+newtype MemoM a = MemoM {
+    runMemoT :: MemoT a 
+   } deriving (Functor, Monad, MonadState Memo)
 
-anagrams :: Dictionary -> [String] -> [[String]]
-anagrams d sentence = anagrams' $ occurrences sentence
+anagrams' :: String -> IO [String]
+anagrams' s = do
+  xs <- readFile "resources/linuxwords.txt"
+  let dict = dictionary . concatMap words . lines $! xs
+      ans  = fmap unwords <$>
+             anagramsM dict (occurrences s)
+  return $ evalMemoM ans $ M Map.empty
+
+evalMemoM :: MemoM a -> Memo -> a
+evalMemoM = evalState . runMemoT
+
+anagramsM :: Dictionary -> Occurrences -> MemoM [[String]]
+anagramsM _ []   = return [[]]
+anagramsM dict occr = do
+    M memo <- get
+    case Map.lookup occr memo of
+      Just xs -> return xs
+      Nothing -> do
+          bs <- forM (filter (/=[]) $ combinations occr) $ \s ->
+                  let pres s = maybe [] id $ wordAnagrams s dict
+                      append s ys = (:ys) <$> pres s
+                  in concatMap (append s) <$>
+                     (anagramsM dict $ subtract occr s)
+          let as = concat bs
+          put $ M $ Map.insert occr as memo
+          return as
+
+anagrams :: Dictionary -> Occurrences -> [String]
+anagrams d occr = unwords <$> go occr
     where          
-        anagrams' :: Occurrences -> [[String]]
-        anagrams' [] = [[]]
-        anagrams' occr
-            = concatMap mkAnagrams $ filter (/=[]) $ combinations occr
-          where
-              mkAnagrams :: Occurrences -> [[String]]
-              mkAnagrams soccr 
-                  = concatMap (append soccr) $ suffixes soccr
-              append soccr suffix  = map (:suffix) $ prefixes soccr
-              prefixes soccr = maybe [] id $ wordAnagrams soccr d
-              suffixes soccr = anagrams' $ subtract occr soccr
+        go occr =
+            let mkAnagrams s = concatMap (append s) $ suffixes s
+                append s t = (:t) <$> prefixes s
+                prefixes s = maybe [] id $ wordAnagrams s d
+                suffixes s = go $ subtract occr s
+                xs         = filter (/=[]) $ combinations occr
+            in if null occr then [[]] else concatMap mkAnagrams xs
 
 combinations :: Occurrences -> [Occurrences]
 combinations []             = [[]]
 combinations ((c, f) : cfs)
     = concatMap extendOccs rest ++ rest
   where
-      extendOccs  occs = map (:occs) prefixs
-      rest        = combinations cfs
-      prefixs     = map ((,) c) [1 .. f]
+      extendOccs occs = (:occs) <$> prefixs
+      rest            = combinations cfs
+      prefixs         = (,) c <$> [1 .. f]
 
 subtract :: Occurrences -> Occurrences -> Occurrences
 subtract x []              = x
@@ -70,29 +79,19 @@ subtract ((xc, xf) : xcfs) y @ ((yc, yf) : ycfs)
     | otherwise = (xc, xf) : subtract xcfs y
 
 wordAnagrams :: Occurrences -> Dictionary -> Maybe [String]
-wordAnagrams occr d = lookup occr d
+wordAnagrams occr dict = Map.lookup occr dict
 
-occurrences :: [String] -> Occurrences
-occurrences = map f . group . sortedList
+occurrences :: String -> Occurrences
+occurrences = map f . group . mkSortedList
   where
       f s = (head s, length s)
-      sortedList = sort . map toLower . concatMap id
+      mkSortedList = sort . map toLower . filter (/=' ')
 
 dictionary :: [String] -> Dictionary
-dictionary
-    = map dictEntry . groupBy pairEq . sortBy (comparing fst) . map pairs
+dictionary entries = Map.fromList $ map dictEntry xs
   where
+      xs = groupBy pairEq $ sortBy (comparing fst) $
+           map pairs entries
       dictEntry l = (fst $ head  l, map snd l)
-      pairs s = (occurrences [s], s)
+      pairs s = (occurrences s, s)
       pairEq p1 p2 = fst p1 == fst p2
-
-memoizeM :: 
-    Ord a => ((a -> StateMap a b) -> (a -> StateMap a b)) -> (a -> b)
-memoizeM t x = evalState (f x) Map.empty 
-  where
-      g x = do
-          y <- t f x  
-          m <- get
-          put $ Map.insert x y m;
-          return y
-      f x = get >>= \m -> maybe (g x) return (Map.lookup x m)
